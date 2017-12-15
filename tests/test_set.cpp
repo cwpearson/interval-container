796fad640817e7ba5df38eb2eb8e0319c1bcbb8a
Carl Pearson <carl.w.pearson@gmail.com>
2017-12-15 12:08:54 -0600

    Fix tests with new interface

--- tests/test_set.cpp
+++ tests/test_set.cpp
@@ -30,13 +30,13 @@ int main(void) {
   IntervalSet<Interval<int>> s;
   s.insert_join(i0);
   assert(s.end() != s.find(i1));
-  assert(*s.find(i1) == i0);
+  assert(**s.find(i1) == i0);
   assert(s.end() != s.find(i2));
-  assert(*s.find(i2) == i0);
+  assert(**s.find(i2) == i0);
   assert(s.end() != s.find(i3));
-  assert(*s.find(i3) == i0);
+  assert(**s.find(i3) == i0);
   assert(s.end() != s.find(i4));
-  assert(*s.find(i4) == i0);
+  assert(**s.find(i4) == i0);
   std::cerr << "pass 3\n\n";
   }
 
@@ -48,11 +48,12 @@ int main(void) {
   s.insert_join(i0);
   s.insert_join(i1);
   assert(s.size() == 1);
-  assert(Interval<int>(1,4) == *s.find(i4));
-  assert(Interval<int>(1,4) == *s.find(i2));
+  assert(Interval<int>(1,4) == **s.find(i4));
+  assert(Interval<int>(1,4) == **s.find(i2));
   assert(s.end() == s.find(i3));
   std::cerr << "pass 4\n\n";
 }
+
 
 {  
   std::cerr << "run 5...\n";
@@ -60,16 +61,16 @@ int main(void) {
   Interval<int> i0(1,3), i1(3,5), i2(2,4);
   s.insert_join(i0);
   assert(s.size() == 1);
-  assert(i0 == *s.find(i0));
-  assert(i0 == *s.find(i2));
+  assert(i0 == **s.find(i0));
+  assert(i0 == **s.find(i2));
   assert(s.find(i0) == s.find(i2));
   assert(s.end() == s.find(i1));
 
   s.insert_join(i1);
   assert(s.size() == 2);
-  assert(i0 == *s.find(i0));
-  assert(i1 == *s.find(i1));
-  assert(i0 == *s.find(i2) || i1 == *s.find(i2));
+  assert(i0 == **s.find(i0));
+  assert(i1 == **s.find(i1));
+  assert(i0 == **s.find(i2) || i1 == **s.find(i2));
 
   auto ii = s.find(i0);
   auto jj = s.find(i1);
@@ -79,9 +80,9 @@ int main(void) {
   assert(s.size() == 1);
   {
   Interval<int> expected(1,5);
-  assert(expected == *s.find(i0));
-  assert(expected == *s.find(i1));
-  assert(expected == *s.find(i2));
+  assert(expected == **s.find(i0));
+  assert(expected == **s.find(i1));
+  assert(expected == **s.find(i2));
   }
   std::cerr << "pass 5\n\n";
 }
@@ -103,7 +104,7 @@ int main(void) {
   IntervalSet<Interval<int>> s;
   s.insert_join(i0);
   s.insert_join(i1);
-  assert(s.erase(i2));
+  assert(s.erase(i0));
   assert(s.size() == 0);
   std::cerr << "pass 7\n\n";
   }
@@ -115,7 +116,7 @@ int main(void) {
   s.insert_join(i1);
   s.insert_join(i2);
   assert(s.size() == 1);
-  assert(i1 == *s.find(Interval<int>(18,19)));
+  assert(i1 == **s.find(Interval<int>(18,19)));
   std::cerr << "pass 7.5\n\n";
   }
 
@@ -139,8 +140,8 @@ int main(void) {
 
   s.insert_split(i2);
   assert(s.size() == 1);
-  assert(i2 == *s.find(i2));
-  assert(i2 == *s.find(i0));
+  assert(i2 == **s.find(i2));
+  assert(i2 == **s.find(i0));
   std::cerr << "pass 9\n\n";
   }
 
@@ -151,9 +152,9 @@ int main(void) {
   s.insert_split(i0);
   s.insert_split(i1);
   assert(s.size() == 3);
-  auto f = *s.find(Interval<int>(0,1));
+  auto f = **s.find(Interval<int>(0,1));
   std::cerr << f.lower() << " " << f.upper() << "\n";
-  assert(Interval<int>(0,1) == *s.find(Interval<int>(0,1)));
+  assert(Interval<int>(0,1) == **s.find(Interval<int>(0,1)));
 
   // s.insert_split(i2);
   // assert(s.size() == 1);
